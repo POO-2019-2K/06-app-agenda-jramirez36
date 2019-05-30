bda8422f5186637f0960367383d5252b0a860359
jramirez36 <42725506+jramirez36@users.noreply.github.com>
2019-05-30 08:56:34 -0500

    optimizado ultima entrega
    sin comentarios y reducción de nombres

--- agenda/Agenda.js
+++ agenda/Agenda.js
@@ -1,39 +1,28 @@
-//se importa la informacion de contactos para poder ser utilizada
 import Contactos from "./Contacto.js";
-//comando export para poder exportar la clase
 export default class Agenda {
-    //constructor donde se resiven las tablas en las que se imprimira
-    constructor(tablaAgenda, tablaNumeroContactos) {
+    constructor(tablaAgenda, NumContactos) {
     this._tablaAgenda = tablaAgenda;
-    this._tablaNumeroContactos = tablaNumeroContactos;
-    //variable array para contener nuestras variables antes de guardar en localStorage
+    this._NumContactos = NumContactos;
     this._Contactos = [];
     this._numCont = 0;
-    //el iniciador sirve para revisar el localStorage
-    this._iniciadorDeTablas();
+    this._iTablas();
 }
-
-    _iniciadorDeTablas() 
+    //iTablas es un iniciador para ver el localstorage
+    _iTablas() 
     {
-        //comando para limpiar el localStorage
     //localStorage.removeItem("Almacen")
-    // se guarda la informacion en un formato let para poder ser comparado y utulizado
-    let listaDeContactos = JSON.parse(localStorage.getItem("Almacen"));
-    console.log(listaDeContactos)
-    //se usa el if para de estar vacio el localStorage no se llegue a imprimir nada
-    if (listaDeContactos === null) {
+    let lContactos = JSON.parse(localStorage.getItem("Almacen"));
+    console.log(lContactos)
+    if (lContactos === null) {
     return;
     }
-    //se usa un forEach para poder revisar todos los datos guardados en el localStorage
-    listaDeContactos.forEach((Contacto, index) => {
-        //de encontrarse informacion se manda a llamar la funcion _AgregarAtabla con la informacion de Contactos
-        this._AgregarATabla(new Contactos(Contacto));
+    lContactos.forEach((Contacto, index) => {
+        this._ATabla(new Contactos(Contacto));
     })
     }
-    //Se resiven valores de contacto
-    _AgregarATabla(Contacto) 
+    //agregar tabla
+    _ATabla(Contacto) 
     {
-        //se comienza a insertar informacion en tablaAgenda
     let row = this._tablaAgenda.insertRow(-1);
     let celdaNombre = row.insertCell(0);
     let celdaCorreo = row.insertCell(1);
@@ -43,19 +32,16 @@ export default class Agenda {
     let celdaDescripcion = row.insertCell(5);
     row.insertCell(6);
     row.insertCell(7);
-    //se imprime la informacion
     celdaNombre.innerHTML = Contacto.Nombre;
     celdaCorreo.innerHTML = Contacto.Correo;
     celdaNumero .innerHTML = Contacto.Numero;
-    celdaCumple.innerHTML = Contacto._OptenerCumpleEnFormatoDatoCadena();
-    celdaEdad.innerHTML = Contacto._OptenerEdad();
+    celdaCumple.innerHTML = Contacto._OpCadena();
+    celdaEdad.innerHTML = Contacto._OpEdad();
     celdaDescripcion.innerHTML = Contacto.Descripcion;
-    let Edad = Contacto._OptenerEdad();
+    let Edad = Contacto._OpEdad();
     this._numCont++;
-    this._tablaNumeroContactos.rows[0].cells[1].innerHTML = this._numCont;
-    //funcion para agregar botones
-    this._AgregarEditarEliminarFilaEnTabla(row, Contacto); 
-    //se guarda la informacion que se acaba de imprimir para hacer un espejo con contacto.js y no se reinicie el array
+    this._NumContactos.rows[0].cells[1].innerHTML = this._numCont;
+    Contacto._Boton(row, Contacto); 
     let objContactos = {
         Nombre: Contacto.Nombre,
         Correo: Contacto.Correo,
@@ -64,18 +50,14 @@ export default class Agenda {
         Descripcion: Contacto.Descripcion,
         Edad: Edad
                         };
-       //se sube la informacion en nuestra variable contactos la cual esta haciendo espejo con la pagina Contacto.js
     this._Contactos.push(objContactos);
     }
-    //funcion para comprobar que el contacto no esta agregado
-    _AgregarContacto(Contacto) 
+    //agregar contacto
+    _AContacto(Contacto) 
     {
-    let Encontrado = Contacto._RevisionContacto(this._Contactos, Contacto.Nombre);
-    console.log(Encontrado)
-    //en caso de encontrado ser mayor o igual a 0 esto quiere decir que si esta registrado
+    let Encontrado = Contacto._RContacto(this._Contactos, Contacto.Nombre);
     if(Encontrado >= 0)
     {
-        //mensaje de error estilo bootstrap
         Swal.fire({
         type: "error" ,
         title: "Error",
@@ -83,71 +65,27 @@ export default class Agenda {
         })
         return;
     }
-    //en caso de pasar la prueba se imprime en _AgregarAtabla
-    this._AgregarATabla(Contacto);
+    this._ATabla(Contacto);
     localStorage.setItem("Almacen", JSON.stringify(this._Contactos));
-    }
-
-    //parte de funciones de botones
-    _AgregarEditarEliminarFilaEnTabla(row, Contacto)
-    {
-        //se agrega boton Editar
-        let btnEditar = document.createElement("input");
-        //tipo de imput se selecciona button de boton
-        btnEditar.type = "button";
-        //se le da un nombre con el que saldra en este caso editar
-        btnEditar.value = "Editar";
-        //se seleciona gracias a bootstrap su estilo
-        btnEditar.className = "btn btn-success";
-        //se le asigna una evento al cliquear
-        btnEditar.addEventListener("click", () => {
-            //la funcion se activara y ejecutara el comando el cual redireciona ala funcion editarFila
-            //se manda los datos con los que trabajara la funcion 
-        Contacto._editarFila(row, Contacto);
-        })
-        //se agrega boton Eliminar
-        let btnEliminar = document.createElement("input");
-        //tipo de imput se selecciona button de boton
-        btnEliminar.type = "button";
-        //se le da un nombre con el que saldra en este caso eliminar
-        btnEliminar.value = "Eliminar";
-        //se seleciona gracias a bootstrap su estilo
-        btnEliminar.className = "btn btn-danger";
-        //se le asigna una evento al cliquear
-        btnEliminar.addEventListener("click", () => {
-            //la funcion se activara y ejecutara el comando el cual redireciona ala funcion editarFila
-            //se manda los datos con los que trabajara la funcion 
-            Contacto._Eliminar(row, Contacto);
-        })
-        //se da un espacio en blanco en la celda en la cual se colocara el boton para que quede limpia 
-        row.cells[6].innerHTML = "";
-        //se agrega el boton Editar
-        row.cells[6].appendChild(btnEditar);
-        //se da un espacio en blanco en la celda en la cual se colocara el boton para que quede limpia 
-        row.cells[7].innerHTML = "";
-        //se agrega el boton Eliminar
-        row.cells[7].appendChild(btnEliminar);
     }
     _Ordenar(Tipo)
         {
             var Ordenados = this._Contactos.slice(-this._numCont);
             if (Tipo === 1)
             {
-                //comando que permite ordenar
                 Ordenados.sort(function(a, b) {
                 return a.Nombre.localeCompare(b.Nombre);
                                                                 });
             }
         else if (Tipo === 2)
             {
-                //comando que permite ordenar
                 Ordenados.sort(function(a, b) {
                 return a.Edad - b.Edad;
                                                                 });
             }
             this._Limpiador();
             localStorage.setItem("Almacen", JSON.stringify(Ordenados));
-            this._iniciadorDeTablas();
+            this._iTablas();
         }
     _Limpiador()
         {
--- agenda/Main.js
+++ agenda/Main.js
@@ -1,29 +1,21 @@
-//nuestra pagina Main que redirige toda la informacion
-//importacion de clases para su uso en el contructor
 import Contactos from "./Contacto.js";
 import Agenda  from "./Agenda.js";
 class Main {
 constructor() 
 {
-    //guardado de tabla Agenda y NumeroContactos para su uso en la pagina Agenda.js
-    let TablasAgenda = new Agenda( document.querySelector("#Agenda"), document.querySelector("#NumeroContactos"));
-    //funcion para el boton Agregar
+    //TAgenda es tablas de agenda
+    let TAgenda = new Agenda( document.querySelector("#Agenda"), document.querySelector("#NumeroContactos"));
     document.querySelector("#Agregar").addEventListener("click", () => {
     let form = document.querySelector("#form");
-    // validacion para informacion
     if (form.checkValidity() === true) 
     {
-        //invocacion de los valores necesarios
         let Nombre = document.querySelector("#nombre").value;
         let Correo = document.querySelector("#correo").value;
         let Numero = document.querySelector("#numero").value;
-        // se usa split para la separación de la cadena.
         let CumpleInicial = document.querySelector("#cumple").value;
         CumpleInicial = CumpleInicial.split("-");
-        // se rearma la cadena pero en la posicion que se ocupa 
         let Cumple = new Date(CumpleInicial[0], CumpleInicial[1] - 1, CumpleInicial[2]);
         let Descripcion = document.querySelector("#descripcion").value;
-        // se crea nuestro paquete de datos
         let objContactos = {
         Nombre: Nombre,
         Correo: Correo,
@@ -31,11 +23,8 @@ constructor()
         Cumple: Cumple,
         Descripcion: Descripcion
         };
-        // se manda el paquete de datos a nuestra pagina contacto para que se hagan las funciones
         let Contacto = new Contactos(objContactos);
-        //se manda la informacion que se mandara a contacto a la funcion AgregarContacto dentro de tablasAgenda para agregar contactos
-        //aqui se agregan contactos 
-        TablasAgenda._AgregarContacto(Contacto);
+        TAgenda._AContacto(Contacto , TAgenda);
     }
     else
     {
@@ -67,10 +56,8 @@ constructor()
                 text: "No selecciono en que orden se ordenara la lista por lo cual no se puede continuar"
                 })
         }
-        //se invoca la funcion ordenar
-            TablasAgenda._Ordenar(Tipo);
+            TAgenda._Ordenar(Tipo);
         });
 } 
 }
-//se da inicio al Main
 new Main();
